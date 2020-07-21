58210654f6ce2f98205fa90452b695fb14dedfe9
igor-the-developer <igorstrelkov95@gmail.com>
2020-07-21 15:54:11 +0100

    Updates

---
+++ precache-manifest.9e224c7206f0a98009ea9adc9e309aa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be1a34885a913d5bd3e6a6e7b9dadea4",
+    "url": "/scan/index.html"
+  },
+  {
+    "revision": "ffe76ba04bab8ca1bda8",
+    "url": "/scan/static/css/main.d0a58c27.chunk.css"
+  },
+  {
+    "revision": "1d2af8f564007dd87fd6",
+    "url": "/scan/static/js/2.3f48c5bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scan/static/js/2.3f48c5bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffe76ba04bab8ca1bda8",
+    "url": "/scan/static/js/main.d55c5686.chunk.js"
+  },
+  {
+    "revision": "2df7b1e15393664230d3",
+    "url": "/scan/static/js/runtime-main.a8768137.js"
+  }
+]);
